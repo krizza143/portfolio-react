95527319fe27c55b6dd2d4cb77d86fbcd64852f6
Krizza Barzaga <kimbarzaga@gmail.com>
2021-02-13 11:44:40 +0800

    Banner Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import './scss/style.scss';
+import Particles from 'react-particles-js';
+import Banner from './components/Banner'
 
 function App() {
   return (
-    <h1>Hi Bitch</h1>
+	<>
+	<Particles 
+		params={{
+			particles:{
+				number: {
+					value: 30,
+					density: {
+						enable: true,
+						value_area: 900
+					}
+				},
+				shape: {
+					type: "circle",
+					stroke: {
+						width: 2,
+						color: "#ABABAB"
+					}
+				},
+				line_linked: {
+						color: "none"
+				}
+			}
+		}}
+	/>
+	<Banner />
+	</>
   );
 }
 
---
+++ src/components/Banner.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Typed from 'react-typed';
+
+const Banner = () => {
+    return (
+        <div className="banner container-fluid">
+            <div className="row text-lg-left text-md-center text-sm-center p-lg-5">
+                <div className="col-12 animated fadeInDown">
+                    <p className="mb-1">Hi! Welcome to my portfolio, I am</p>
+                    <h1>
+                   <Typed 
+                        className="mb-2 typed-text"
+                        strings={['Krizza Barzaga']}
+                        typeSpeed={60}
+                        onComplete={(self) => self.cursor.remove()}
+                    />
+                    </h1>
+                    <p className="mb-lg-4 mb-md-2 mb-sm-3">FRONT-END WEB DESIGNER</p>
+                        <div className="col-lg-12 col-md-7 col-sm-6 mx-auto p-0">
+                            <a className="btn btn-outline m-md-2" href="#aboutme"><p>ABOUT ME</p></a>
+                            <a className="btn btn-outline m-md-2" href="#works"><p>WORKS</p></a>
+                            <a className="btn btn-outline m-md-2" href="#skills"><p>SKILLS</p></a>
+                            <a className="btn btn-outline m-md-2" href="#contact"><p>CONTACT</p></a>
+                        </div>
+                </div>
+            </div>
+	    </div>
+    )
+}
+
+export default Banner
